4cf928d56d5324b28e05bf14e312601861ddae9c
Darmokhval Viktor <darmokhvalviktor@gmail.com>
2024-05-07 11:24:28 +0300

    Initial commit

---
+++ src/main/java/com/darmokhval/crm_programming_school/CrmProgrammingSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.darmokhval.crm_programming_school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmProgrammingSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmProgrammingSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/darmokhval/crm_programming_school/CrmProgrammingSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.darmokhval.crm_programming_school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmProgrammingSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
